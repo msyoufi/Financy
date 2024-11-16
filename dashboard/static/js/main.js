a70f48cc86f9d42123a04a1731a1f45d402d38e3
msyoufi <msyoufi.dev@gmail.com>
2024-11-16 12:44:37 +0100

    Imporved design.
    - Responsive auth pages.
    - Changed the color theme

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -1,4 +1,4 @@
-// Keep the label of an input at the top when input is not empty
+// Keep the input's label at top when not empty
 document.querySelectorAll('.financy-input').forEach(input => {
   input.addEventListener('input', () => {
     const label = input.parentElement.querySelector('label');
